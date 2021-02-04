83195013982140acefdfb21357e42ad33ca23cf8
Wilsdantas <wilsdantas@gmail.com>
2021-02-04 20:07:34 -0300

    store category

--- src/pages/Category/_partials/dialog.vue
+++ src/pages/Category/_partials/dialog.vue
@@ -1,25 +1,24 @@
 <template>
   <div class="text-center">
-    <v-dialog v-model="dialog" width="500">
+    <v-dialog v-model="dialog" width="300">
       <template v-slot:activator="{ on, attrs }">
         <v-btn color="primary" dark v-bind="attrs" v-on="on"> New Item </v-btn>
       </template>
-
+      <v-form ref="form" @submit="storeCategory" @submit.prevent>
       <v-card>
         <v-card-title class="headline">
-          <v-text-field
-            v-model="name"
-            label="Name"
-            single-line
-            hide-details
-          ></v-text-field>
+          <div class="red--text" v-if="errors.name != undefined">
+            <h6>{{ this.errors.name[0] }}</h6>
+          </div>
+          <v-text-field v-model="formData.name" label="Name"></v-text-field>
         </v-card-title>
 
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="dialog = false"> Add </v-btn>
+          <v-btn color="primary" type="submit" text> Add </v-btn>
         </v-card-actions>
       </v-card>
+      </v-form>
     </v-dialog>
   </div>
 </template>
@@ -27,8 +26,23 @@
 <script>
 export default {
   data: () => ({
-    name: "",
+    formData: {},
+    errors: {},
     dialog: false,
   }),
+
+  methods: {
+    storeCategory() {
+      this.$store
+        .dispatch("storeCategory", this.formData)
+        .then((response) => {
+          this.dialog = false;
+          this.$store.dispatch("categories")
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
 };
 </script>
--- src/store/modules/categories/actions.js
+++ src/store/modules/categories/actions.js
@@ -13,4 +13,16 @@ export default {
                 .finally(() => context.commit('SET_PRELOADER', false))
         })
     },
+
+    storeCategory(context, params) {
+        context.commit('SET_PRELOADER', true)
+        context.commit('SET_TEXT_PRELOADER', 'Cadastrando Categoria...')
+        return new Promise((resolve, reject) => {
+            axios.post('categories', params)
+                .then(response => {
+                    resolve()
+                }).catch(error => reject(error))
+                .finally(() => context.commit('SET_PRELOADER', false))
+        })
+    },
 }
